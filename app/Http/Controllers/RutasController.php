2ad6723f0517a3b335d34ca4046e58a9297a01dd
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-26 04:19:39 +0200

    creadas varias vistas y rutas con controller

---
+++ app/Http/Controllers/RutasController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RutasController extends Controller
+{
+    public function inicio(){
+        return view('welcome');
+    }
+
+    public function buscadores(){
+        return view('buscadores');
+    }
+
+    public function prensa(){
+        return view('prensa');
+    }
+
+    public function rrss(){
+        return view('rrss');
+    }
+
+    public function compras(){
+        return view('compras');
+    }
+
+    public function bancos(){
+        return view('bancos');
+    }
+
+    public function video(){
+        return view('video');
+    }
+
+
+
+
+
+}
+
+
