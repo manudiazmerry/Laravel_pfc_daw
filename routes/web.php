f25bb40dde9b68092bd47aa7c7a20a0248d1af10
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-28 00:34:19 +0200

    generado sistema de registro y login

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/index', 'RutasController@inicio');
 Route::get('/filtro/{condi}', 'RutasController@filtro');
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
