255056754a5ab52123d2e858a6cd46987c5f282d
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-28 18:21:59 +0200

    logo y varios más

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,22 @@
 @section("contenido")
 
 <div class="container">
+<IMG SRC='C:/Users/manud/code/Laravel_pfc_daw/public/images/mopli.gif'>
     <div class="row">
     @foreach($paginas as $pagina)
+
+
+    <div style="width:300px; height:85px; border:1px solid blue; background:white; border-radius:15px; padding:15px 25px; margin:10px;">
         <a href='http://{{$pagina->url}}' target="_blank">
-            <div class="col">{{$pagina->nombre}}</div>
+            <div>
+            <IMG SRC='http://{{$pagina->url}}/favicon.ico' height="32px" style="border:1px solid blue;">
+            <span style="border:1px solid blue; padding-left:21px; font-family:Passion one; font-size:24px">{{$pagina->nombre}}</span>
+            </div>
        </a>
+    </div>
+
+
+
     @endforeach
     </div>
 </div>
@@ -22,3 +33,6 @@
 
 @section("pie")
 @endsection
+
+
+
