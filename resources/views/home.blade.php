cc636d7fd4172fe0f73055600e44098a39527ae1
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-30 19:46:49 +0200

    redirección y mensaje bienvenida

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+<head>
+  <meta http-equiv="Refresh" content="3; URL=http://pfc_daw.test/">
+</head>
+
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header" style="text-align:left; color:grey;">{{ __('Mensaje') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="text-align:center; color:#0b5cd6; font-size:1.7em;">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('¡ BIENVENID@ !') }}
                 </div>
             </div>
         </div>
