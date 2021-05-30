1ed55d122c1560945c3611ed00ab66898a55c463
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-30 04:45:46 +0200

    Versión 1.2

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -47,14 +47,11 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="background-color:#f8f9fa;text-align: right;">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
         <!--nav item-------------------------->
-        <style>
-          .nav-item{background-color:#f8f9fa; }
-          .nav-link{text-align: right;}
-        </style>
+        
         
         <li class="nav-item">
           <a class="nav-link" href="/filtro/buscadores">Buscadores</a>
