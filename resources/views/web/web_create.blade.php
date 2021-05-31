b339257d8403b11401ccdddf87e11ec17b166531
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-31 04:06:10 +0200

    version 2.0

--- resources/views/web/web_create.blade.php
+++ resources/views/web/web_create.blade.php
@@ -28,7 +28,7 @@
                 <div class="form-group row">
                     <label class="col-md-4 col-form-label text-md-right">Nombre</label>
                     <div class="col-md-6">
-                        <input id="nombre" name="nombre" type="text" class="form-control" value="" required autocomplete="" autofocus>
+                        <input id="nombre" name="nombre" type="text" class="form-control" value="" required autocomplete="" maxlength="17" autofocus>
                     </div>
                 </div>
 
