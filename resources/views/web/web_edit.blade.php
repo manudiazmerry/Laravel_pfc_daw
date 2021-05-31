e6b8eb472a310bebc3f8ddfb991d046c73d6dda8
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-31 04:12:43 +0200

    favicon

---
+++ resources/views/web/web_edit.blade.php
@@ -0,0 +1,63 @@
+
+@extends("../layouts.plantilla")
+
+@section("cabecera")
+<!--ver navbar-->
+@endsection
+
+
+@section("contenido")
+
+<div class="container">
+    <!--------------------------------- mensaje---------->
+    <p class="txt_ruta">Añadir</p>
+
+
+
+    <!--------------------------------- form---------->
+
+    <div class="card">
+        <div class="card-header">Añade un link personalizado</div>
+
+        <div class="card-body">
+
+            <form method="GET" action="/webs/store">
+            @csrf <!---- directiva crea token para evitar los errores 419 http de identificación en servidor, cosas geniales de laravel! ------->
+                
+
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">Nombre</label>
+                    <div class="col-md-6">
+                        <input id="nombre" name="nombre" type="text" class="form-control" value="" required autocomplete="" maxlength="17" autofocus>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">url</label>
+                    <div class="col-md-6">
+                        <input id="url" name="url" type="text" class="form-control" required autocomplete="">
+                    </div>
+                </div>
+             
+                <div class="form-group row mb-0">
+                    <div class="col-md-8 offset-md-4">
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                        <a href="/webs" class="btn btn-danger">Cancelar</a>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    
+
+</div>
+
+@endsection
+
+
+@section("pie")
+<!--ver pie-->
+@endsection
+
