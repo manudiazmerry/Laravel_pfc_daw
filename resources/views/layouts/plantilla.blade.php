ddadcb9155278f789d2d4f08482497b390d95dee
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2021-05-31 04:13:26 +0200

    favicon

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PFC DAW Manu</title>
 
+    <link rel="shortcut icon" href="/favicon.ico" />
+
+
     <!---enlazando estilos css por este orden: bootstrap online (comentado), bootstrap, bs modificado, propio--------------------------------------------------->
-
-
     <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">--->
     <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
